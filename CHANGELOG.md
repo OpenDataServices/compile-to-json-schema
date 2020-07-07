9b7f6cd5791c9713fb323e2e4ba6d3bb101cc988
James <james.baster@opendataservices.coop>
2020-07-07 14:45:57 +0100

    release: 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [0.2.0] - 2020-07-07                                                 
+
 ### Fixed
 
 - Corrected Spelling of package name in setup.py
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info[0] >= 3 and sys.version_info[1] >= 6:
 
 setup(
     name="compiletojsonschema",
-    version="0.1.0",
+    version="0.2.0",
     author="Open Data Services",
     author_email="code@opendataservices.coop",
     url="https://github.com/OpenDataServices/compile-to-json-schema",
